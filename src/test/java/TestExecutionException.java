7bbf8d4690274ac614dbe37ee94a6a01bf6e7d4b
Nea1in <Sergiy19755791@gmail.com>
2024-08-23 00:18:38 +0300

    removed an unnecessary method

--- src/test/java/TestExecutionException.java
+++ src/test/java/TestExecutionException.java
@@ -1,7 +1,5 @@
 public class TestExecutionException extends RuntimeException {
-    public TestExecutionException(String message) {
-        super(message);
-    }
+
 
     public TestExecutionException(String message, Throwable cause) {
         super(message, cause);
